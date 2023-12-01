f50f293eec4d516cb31430dac488ff19be33b7c9
Zaps6000 <122732007+Zaps6000@users.noreply.github.com>
2023-11-30 22:23:47 -0600

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,34 @@
+Config = {
+    Requirement = "scammer", -- Job Requirement
+    BlackMarketLocations = {
+        {
+            x = 149.5987, 
+            y = -1055.6990,
+            z = 29.1986,
+            name = "Black Market",
+            items = {
+                { itemName = "MSR", price = 5000 },
+                { itemName = "Card", price = 100 },
+            },
+        },
+    },
+    ElectronicStoreLocations = {
+        {
+            x = 200.0,
+            y = -800.0,
+            z = 15.0,
+            name = "Electronic Store",
+            items = {
+                { itemName = "laptop", price = 50 },
+            },
+        },
+    },
+    CashCheckingLocations = {
+        {
+            x = 251.8756,
+            y = 221.6036,
+            z = 106.2866,
+            name = "Cash Checking",
+        },
+    },
+}
