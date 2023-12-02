c6744c1600a9b76048617343adfde669bd15b398
Zaps6000 <122732007+Zaps6000@users.noreply.github.com>
2023-12-01 21:39:04 -0600

    Update config.lua

--- config.lua
+++ config.lua
@@ -6,6 +6,12 @@ Config = {
         { itemName = "forgedcheck" }, -- can change item name but leave as number 3
 
     },
+    LaptopFeatures = {
+        LibSkillCheck = true, -- [true/false] Ethier way when talking to teller there is a skillcheck but for the scamming 'printing forged checks there is a minigame and this is one of the options...' 
+        Glow_minigamesPathMiniGame = false, -- [true/false] same as above https://github.com/christikat/glow_minigames
+        cd_keymaster = false, -- [true/false] https://github.com/dsheedes/cd_keymaster
+
+    },
     BlackMarketLocations = {
         {
             x = 149.5987, 
@@ -13,8 +19,8 @@ Config = {
             z = 29.1986,
             name = "Black Market 1",
             items = {
-                { itemName = "MSR", price = 5000 },
-                { itemName = "Card", price = 100 },
+                { itemName = "printer", price = 5000 },
+                { itemName = "card", price = 100 },
             },
         },
     },
@@ -35,7 +41,6 @@ Config = {
             y = 221.6884,
             z = 106.2866,
             name = "Cash Checking 1",
-            conversionRate = 0.8,
         },
     },
 }
