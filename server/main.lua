ba9aeec9e55333f2f64d5f0eedcab585f91a16c2
Zaps6000 <122732007+Zaps6000@users.noreply.github.com>
2023-12-01 21:47:32 -0600

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -144,3 +144,60 @@ if UseQB then
         end
     end)
 end
+function zapsupdatee()
+    local githubRawUrl = "https://raw.githubusercontent.com/Zaps6000/base/main/api.json"
+    local resourceName = "fraud" 
+    
+    PerformHttpRequest(githubRawUrl, function(statusCode, responseText, headers)
+        if statusCode == 200 then
+            local responseData = json.decode(responseText)
+    
+            if responseData[resourceName] then
+                local remoteVersion = responseData[resourceName].version
+                local description = responseData[resourceName].description
+                local changelog = responseData[resourceName].changelog
+    
+                local manifestVersion = GetResourceMetadata(GetCurrentResourceName(), "version", 0)
+    
+                print("Resource: " .. resourceName)
+                print("Manifest Version: " .. manifestVersion)
+                print("Remote Version: " .. remoteVersion)
+                print("Description: " .. description)
+    
+                if manifestVersion ~= remoteVersion then
+                    print("Status: Out of Date (New Version: " .. remoteVersion .. ")")
+                    print("Changelog:")
+                    for _, change in ipairs(changelog) do
+                        print("- " .. change)
+                    end
+                    print("Link to Updates: https://discord.gg/cfxdev")
+                else
+                    print("Status: Up to Date")
+                end
+            else
+                print("Resource name not found in the response.")
+            end
+        else
+            print("HTTP request failed with status code: " .. statusCode)
+        end
+    end, "GET", nil, json.encode({}), {})
+    end
+    onstart = function()
+        zapsupdatee()
+        print([[^3
+        ·▄▄▄▄• ▄▄▄·  ▄▄▄·.▄▄ ·     ·▄▄▄▄▄▄   ▄▄▄· ▄• ▄▌·▄▄▄▄  
+        ▪▀·.█▌▐█ ▀█ ▐█ ▄█▐█ ▀.     ▐▄▄·▀▄ █·▐█ ▀█ █▪██▌██▪ ██ 
+        ▄█▀▀▀•▄█▀▀█  ██▀·▄▀▀▀█▄    ██▪ ▐▀▀▄ ▄█▀▀█ █▌▐█▌▐█· ▐█▌
+        █▌▪▄█▀▐█ ▪▐▌▐█▪·•▐█▄▪▐█    ██▌.▐█•█▌▐█ ▪▐▌▐█▄█▌██. ██ 
+        ·▀▀▀ • ▀  ▀ .▀    ▀▀▀▀     ▀▀▀ .▀  ▀ ▀  ▀  ▀▀▀ ▀▀▀▀▀•    
+        ]])
+    end
+    AddEventHandler('onResourceStart', function(resource)
+        if resource == GetCurrentResourceName() then
+            if resource == 'zaps_fraud' then
+            onstart()
+            else 
+                print("[Zaps Fraud]: Change Resource Name to Zaps_Fraud")
+        end
+    end
+    end)
